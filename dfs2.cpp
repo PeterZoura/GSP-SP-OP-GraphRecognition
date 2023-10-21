10ca717b8a36009ae3e31d648375f073df5fc81e
Peter Zoura <fredw401@gmail.com>
2023-10-21 18:59:13 -0400

    Small changes to incorporate slight optimizations suggested by chatgpt website

--- dfs2.cpp
+++ dfs2.cpp
@@ -1,6 +1,4 @@
 #include <vector>
-#include <memory>
-#include <utility>
 #include <iostream>
 #include <fstream>
 #include <stack>
@@ -9,26 +7,22 @@
 
 using std::pair;
 using std::vector;
-using std::unique_ptr;
 
 namespace s=std;
 
 
-void create_edges(int const & num_v, int const & num_e, vector<pair<int, int>> & edges, s::ifstream & istream){
-    for (int i =0; i<num_e ; i++){
+void create_edges(int num_v, int num_e, vector<pair<int, int>>& edges, s::ifstream& istream) {
+    edges.reserve(num_e);
+    for (int i = 0; i < num_e; ++i) {
         int x, y;
         istream >> x >> y;
-        edges.push_back(pair<int,int>(x, y));
+        edges.emplace_back(x, y);
     }
 }
 
-
-void create_adjacency_list(int const & n, int const & m, auto const & edges, auto & adj){
-    for(int i = 0; i<n; i++){
-        adj.emplace_back();
-    }
-    
-    for(pair<int, int> e : edges){
+void create_adjacency_list(int const n, vector<pair<int, int>> const & edges, vector<vector<int>>& adj) {
+    adj.resize(n);
+    for (const auto& e : edges) {
         int x = e.first;
         int y = e.second;
         adj[x].emplace_back(y);
@@ -36,49 +30,51 @@ void create_adjacency_list(int const & n, int const & m, auto const & edges, aut
     }
 }
 
-void dfs(int const & starting_vertex, vector<int> & dfsRank, auto & adj){
+void dfs(int starting_vertex, vector<int>& dfsRank, const vector<vector<int>>& adj) {
     s::stack<int> the_stack;
     the_stack.push(starting_vertex);
-    dfsRank[starting_vertex]=1;
+    dfsRank[starting_vertex] = 1;
     int rank = 2;
-    while(!the_stack.empty()){
-        int descend = 0;
+
+    while (!the_stack.empty()) {
         int topOfStack = the_stack.top();
-        for(int w : adj[topOfStack]){
-            if(dfsRank[w]==-1){
-                dfsRank[w]=rank++;
+        bool descend = false;
+
+        for (int w : adj[topOfStack]) {
+            if (dfsRank[w] == -1) {
+                dfsRank[w] = rank++;
                 the_stack.push(w);
-                descend = 1;
+                descend = true;
                 break;
             }
         }
-        if(descend){
+
+        if (descend) {
             continue;
-        }else{
+        } else {
             the_stack.pop();
         }
     }
 }
 
-vector<int> initialize_dfs(int const & n, auto & adj){
+vector<int> initialize_dfs(int n, vector<vector<int>> const & adj) {
     vector<int> dfsRank(n, -1);
     dfs(0, dfsRank, adj);
     return dfsRank;
 }
 
-int main(int argc, char* argv[]){
-    std::ios::sync_with_stdio(false);
+int main(int argc, char* argv[]) {
+    s::ios::sync_with_stdio(false);
 
     s::ifstream is(argv[1]);
     int n, m;
     is >> n >> m;
-    vector<pair<int, int>> edges;
-    edges.reserve(m);
-    vector<vector<int>> adj;
-    adj.reserve(n);
 
-    create_edges(n, m, edges, is);    
-    create_adjacency_list(n, m, edges, adj);
+    vector<pair<int, int>> edges;
+    create_edges(n, m, edges, is);
+
+    vector<vector<int>> adj;
+    create_adjacency_list(n, edges, adj);
 
     /*
     //Print adjacency list for debugging
@@ -91,17 +87,18 @@ int main(int argc, char* argv[]){
     }
     s::cout << '\n';
     */
+   
     auto time_start = s::chrono::steady_clock::now();
     auto dfsRank = initialize_dfs(n, adj);
     auto time_end = s::chrono::steady_clock::now();
-    
+
     s::chrono::duration<double, s::nano> time_elapsed = s::chrono::duration_cast<s::chrono::nanoseconds>(time_end - time_start);
     s::cout << "time elapsed is: " << s::setprecision(4) << time_elapsed.count()/1000 << "μs\n";
-    
-    //print DFS ranks for testing
+
+    // Print DFS ranks for testing
     /*
-    for(int i = 0; i<dfsRank.size(); i++){
-        s::cout << "Vertex " << i << " | " << dfsRank[i] << "\n";
+    for (int i = 0; i < dfsRank.size(); ++i) {
+        cout << "Vertex " << i << " | " << dfsRank[i] << "\n";
     }
     */
 }
