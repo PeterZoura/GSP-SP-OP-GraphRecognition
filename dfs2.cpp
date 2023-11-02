39d53774acd1001d0833940254e7ca66f5a131d2
Peter Zoura <fredw401@gmail.com>
2023-11-02 17:09:53 -0400

    Fixed edge referencing just like in dfs.cpp

--- dfs2.cpp
+++ dfs2.cpp
@@ -10,29 +10,35 @@ using std::pair;
 using std::vector;
 using std::tuple;
 
+struct Edge{
+    int target;
+    bool tree;
+};
+Edge * getEdge(int, int,vector<vector<Edge>> &);
+
 namespace s=std;
 
 
-void create_edges(int num_v, int num_e, vector<tuple<int, int, bool>>& edges, s::ifstream& istream) {
+void create_edges(int num_v, int num_e, vector<pair<int, int>>& edges, s::ifstream& istream) {
     edges.reserve(num_e);
     for (int i = 0; i < num_e; ++i) {
         int x, y;
         istream >> x >> y;
-        edges.emplace_back( (x>y?y:x), (x>y?x:y), false);
+        edges.emplace_back( (x>y?y:x), (x>y?x:y));
     }
 }
 
-void create_adjacency_list(int const n, vector<tuple<int, int, bool>> const & edges, vector<vector<int>>& adj) {
+void create_adjacency_list(int const n, vector<pair<int, int>> const & edges, vector<vector<Edge>>& adj) {
     adj.resize(n);
     for (const auto& e : edges) {
-        int x = s::get<0>(e);
-        int y = s::get<1>(e);
-        adj[x].emplace_back(y);
-        adj[y].emplace_back(x);
+        int x = e.first;
+        int y = e.second;
+        adj[x].emplace_back(y, false);
+        adj[y].emplace_back(x, false);
     }
 }
 
-void dfs(int starting_vertex, vector<int>& dfsRank, const vector<vector<int>>& adj, vector<tuple<int, int, bool>> & edges) {
+void dfs(int starting_vertex, vector<int>& dfsRank, vector<vector<Edge>>& adj) {
     s::stack<int> the_stack;
     the_stack.push(starting_vertex);
     dfsRank[starting_vertex] = 1;
@@ -42,12 +48,12 @@ void dfs(int starting_vertex, vector<int>& dfsRank, const vector<vector<int>>& a
         int topOfStack = the_stack.top();
         bool descend = false;
 
-        for (int w : adj[topOfStack]) {
-            if (dfsRank[w] == -1) {
-                s::erase(edges, s::tuple{(w>topOfStack?topOfStack:w), (w>topOfStack?w:topOfStack), false});
-                edges.emplace_back((w>topOfStack?topOfStack:w), (w>topOfStack?w:topOfStack), true);
-                dfsRank[w] = rank++;
-                the_stack.push(w);
+        for (Edge w : adj[topOfStack]) {
+            if (dfsRank[w.target] == -1) {
+                dfsRank[w.target] = rank++;
+                getEdge(topOfStack, w.target, adj)->tree = true;
+                getEdge(w.target, topOfStack, adj)->tree = true;
+                the_stack.push(w.target);
                 descend = true;
                 break;
             }
@@ -61,10 +67,21 @@ void dfs(int starting_vertex, vector<int>& dfsRank, const vector<vector<int>>& a
     }
 }
 
-vector<int> initialize_dfs(int n, vector<vector<int>> const & adj, vector<tuple<int, int, bool>> & edges) {
+vector<int> initialize_dfs(int n, vector<vector<Edge>> & adj) {
     vector<int> dfsRank(n, -1);
-    dfs(0, dfsRank, adj, edges);
+    dfs(0, dfsRank, adj);
     return dfsRank;
+}
+
+Edge * getEdge(int a, int b, vector<vector<Edge>> & adj){
+    int m = 0;
+    for(auto i : adj[a]){
+        if(i.target==b){
+            return &adj[a][m];
+        }
+        m++;
+    }
+    return nullptr;
 }
 
 int main(int argc, char* argv[]) {
@@ -74,42 +91,36 @@ int main(int argc, char* argv[]) {
     int n, m;
     is >> n >> m;
 
-    vector<tuple<int, int, bool>> edges;
+    vector<pair<int, int>> edges;
     create_edges(n, m, edges, is);
 
-    vector<vector<int>> adj;
+    vector<vector<Edge>> adj;
     create_adjacency_list(n, edges, adj);
 
-    /*
+    auto time_start = s::chrono::steady_clock::now();
+    auto dfsRank = initialize_dfs(n, adj);
+    auto time_end = s::chrono::steady_clock::now();
+    
     //Print adjacency list for debugging
     for(int i =0; i<adj.size();i++){
         s::cout << "List " << i << " is: ";
-        for(int j : adj[i]){
-            s::cout << j << " ";
+        for(Edge j : adj[i]){
+            char c = (j.tree?'T':'B');
+            s::cout << c << j.target << " ";
         }
         s::cout << '\n';
     }
     s::cout << '\n';
-    */
-   
-    auto time_start = s::chrono::steady_clock::now();
-    auto dfsRank = initialize_dfs(n, adj, edges);
-    auto time_end = s::chrono::steady_clock::now();
 
 
     s::chrono::duration<double, s::nano> time_elapsed = s::chrono::duration_cast<s::chrono::nanoseconds>(time_end - time_start);
     s::cout << "time elapsed is: " << s::setprecision(4) << time_elapsed.count()/1000 << "μs\n";
 
-    // Print DFS ranks for testing
+    //Print DFS ranks for testing
     //print tree list
-    // for(tuple<int, int, bool> const & e : edges){
-    //     if(s::get<2>(e) == true){
-    //         s::cout << "Tree edge: (" << s::get<0>(e) << ',' << s::get<1>(e) << ")\n";
-    //     }
-    // }    
-    // s::cout << '\n';
-    // for (int i = 0; i < dfsRank.size(); ++i) {
-    //     s::cout << "Vertex " << i << " | " << dfsRank[i] << "\n";
-    // }
+    s::cout << '\n';
+    for (int i = 0; i < dfsRank.size(); ++i) {
+        s::cout << "Vertex " << i << " | " << dfsRank[i] << "\n";
+    }
     
 }
